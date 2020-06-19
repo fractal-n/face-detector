4ca97e784ee8ba24efb2e06497597c19dc8c5b32
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-19 00:40:34 -0600

    add particles background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
 import "./App.css";
 import "tachyons";
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 700,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceDetector /> */}
     </div>
